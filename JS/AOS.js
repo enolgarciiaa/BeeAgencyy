e103fb34ee392039185150958e595935d9b56c18
Enol García Heres <Enolgarciiaa@gmail.com>
2025-03-25 20:10:09 +0100

    hacemos responsive portatil

--- JS/AOS.js
+++ JS/AOS.js
@@ -1,1 +1,8 @@
-AOS.init();
+AOS.init({
+    offset: 120,
+    duration: 600,
+    easing: 'ease-in-out',
+    once: true,
+    mirror: false,
+    disable: 'mobile', // Ojo: esto desactiva AOS en móviles
+  });
